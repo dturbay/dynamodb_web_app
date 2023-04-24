bf3014623a5327fc077d972988c5c60e79527cac
Denys Turbai <dennis.turbay@gmail.com>
2023-04-23 22:14:54 -0700

    Update infra names and image tag

--- setup-infra/bin/setup-infra.ts
+++ setup-infra/bin/setup-infra.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { SetupInfraStack } from '../lib/setup-infra-stack';
+import { SetupECSFargateClusterStack } from '../lib/setup-infra-stack';
 
 const app = new cdk.App();
-new SetupInfraStack(app, 'ECS-FG-Stack', {
+new SetupECSFargateClusterStack(app, 'ECS-FG-Stack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- setup-infra/lib/setup-infra-stack.ts
+++ setup-infra/lib/setup-infra-stack.ts
@@ -1,20 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecsp from 'aws-cdk-lib/aws-ecs-patterns';
 
-export class SetupInfraStack extends cdk.Stack {
+export class SetupECSFargateClusterStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const vpc = new ec2.Vpc(this, 'ECSVpc', { maxAzs: 2 });
+
     new ecsp.ApplicationLoadBalancedFargateService(this, 'webapp', {
       taskImageOptions: {
-        image: ecs.ContainerImage.fromRegistry('registry.hub.docker.com/tda123123/tda_dynamo_web_app:0.0.1'),
+        image: ecs.ContainerImage.fromRegistry('registry.hub.docker.com/tda123123/tda_dynamo_web_app:0.0.2'),
         containerPort: 8080,
       },
       publicLoadBalancer: true,
       desiredCount: 1,
       cpu: 256,
+      vpc,
     });
   }
 }
