8ffb446c2fad08b39fe4a96e948560646166f268
Denys Turbai <dennis.turbay@gmail.com>
2023-04-24 00:20:48 -0700

    Add dynamodb table creation step into cdk script

--- setup-infra/lib/setup-infra-stack.ts
+++ setup-infra/lib/setup-infra-stack.ts
@@ -3,6 +3,9 @@ import { Construct } from 'constructs';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecsp from 'aws-cdk-lib/aws-ecs-patterns';
+import { AttributeType, Table } from "aws-cdk-lib/aws-dynamodb";
+import { RemovalPolicy } from 'aws-cdk-lib';
+
 
 export class SetupECSFargateClusterStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -10,7 +13,18 @@ export class SetupECSFargateClusterStack extends cdk.Stack {
 
     const vpc = new ec2.Vpc(this, 'ECSVpc', { maxAzs: 2 });
 
-    new ecsp.ApplicationLoadBalancedFargateService(this, 'webapp', {
+    const table = new Table(this, 'Work-table', {
+      tableName: 'Work',
+      partitionKey: {
+        name: "id",
+        type: AttributeType.STRING,
+      },
+      readCapacity: 1,
+      writeCapacity: 1,
+      removalPolicy: RemovalPolicy.DESTROY
+    });
+
+    const ecsFargateService = new ecsp.ApplicationLoadBalancedFargateService(this, 'webapp', {
       taskImageOptions: {
         image: ecs.ContainerImage.fromRegistry('registry.hub.docker.com/tda123123/tda_dynamo_web_app:0.0.2'),
         containerPort: 8080,
@@ -20,5 +34,7 @@ export class SetupECSFargateClusterStack extends cdk.Stack {
       cpu: 256,
       vpc,
     });
+
+    table.grantReadWriteData(ecsFargateService.service.taskDefinition.taskRole);
   }
 }
